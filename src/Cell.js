023e2cc12a8afa7de2f498cf595a44c984e07ca0
Tobias Norton <tnorton20@yahoo.com>
2018-11-20 18:57:34 -0500

    Check cell is dynamic

--- src/Cell.js
+++ src/Cell.js
@@ -4,11 +4,21 @@ class Cell extends Component {
   leftClickCell = event => {
     console.log(`Clicked at [${this.props.row}, ${this.props.column}]`)
     //We can't call the API right here because this isn't where the state lives, and the API changes the state.
-    // this.props.checkCell(this.props.row, this.props.column)
+    this.props.checkCell(this.props.row, this.props.column)
   }
 
+  // rightClickCell = event => {
+  //   console.log(`Flagged at [${this.props.row}, ${this.props.column}]`)
+  //   this.props.flagCell(this.props.row, this.props.column)
+  //   event.preventDefault()
+  // }
+
   render() {
-    return <td onClick={this.checkCell}>{this.props.value}</td>
+    return (
+      <td onClick={this.leftClickCell} onContextMenu={this.rightClickCell}>
+        {this.props.value}
+      </td>
+    )
   }
 }
 
